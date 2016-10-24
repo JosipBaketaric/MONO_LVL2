de670fc6a5a2e6dedca39557fb9d912c18de4769
Your Name <you@example.com>
2016-10-24 10:17:13 +0200

    Formatted project

---
+++ Project.MVC/Project.Service/Interfaces/VehicleServiceInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Interfaces
+{
+    interface VehicleServiceInterface
+    {
+        bool Add(VehicleModel vehicleModel);
+        void Add(VehicleMake vehicleMake);
+        void Edit(VehicleModel vehicleModel);
+        void Edit(VehicleMake vehicleMake);
+        VehicleModel FindByIdModel(int? Id);
+        VehicleMake FindByIdMake(int? Id);
+        IEnumerable GetVehicleModels(string search, int? page, string sortBy);
+        IEnumerable GetVehicleMakes(string search, int? page, string sortBy);
+        IEnumerable GetVehicleMakesAll();
+        void RemoveModel(int? Id);
+        bool RemoveMake(int? Id);
+
+    }
+}
---
+++ Project.MVC/Project.Service/ServiceFiles/VehicleContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Project.Service
+{
+    public class VehicleContext : DbContext
+    {
+        public VehicleContext() : base("VehicleDB")
+        {
+        }
+
+        public DbSet<VehicleMake> VehicleMakes { get; set; }
+        public DbSet<VehicleModel> VehicleModels { get; set; }   
+
+    }
+}
